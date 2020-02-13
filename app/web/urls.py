01edae41c84472f2f3f5165701f19601a982b22d
Maurice Guikema <>
2020-02-13 14:06:57 +0100

    health check added

--- app/web/urls.py
+++ app/web/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from web.core.views import HomePageView
+from web.health.views import health_default
 
 urlpatterns = [
 
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='shome'),
+
+    path('omslagroute/health', health_default),
+
     path('admin/', admin.site.urls),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
