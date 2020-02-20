70b03b36825bbc902727cea96242e7a8c26e123a
Maurice Guikema <>
2020-02-20 10:31:36 +0100

    doc type added

--- app/web/documents/models.py
+++ app/web/documents/models.py
@@ -7,6 +7,14 @@ class Document(models.Model):
         verbose_name=_('Titel'),
         max_length=100,
     )
+    document_type = models.ForeignKey(
+        to='DocumentType',
+        verbose_name='Type document',
+        related_name='type_to_document',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
     uploaded_file = models.FileField(
         verbose_name=_('Bestand'),
         upload_to='uploads'
@@ -27,3 +35,18 @@ class Document(models.Model):
         verbose_name = _('Bestand')
         verbose_name_plural = _('Bestanden')
         ordering = ('-uploaded', )
+
+
+class DocumentType(models.Model):
+    type_name = models.CharField(
+        verbose_name=_('Type naam'),
+        max_length=100,
+    )
+
+    class Meta:
+        verbose_name = _('Bestands type')
+        verbose_name_plural = _('Bestands types')
+        ordering = ('type_name', )
+
+    def __str__(self):
+        return self.type_name
