3794d5241be394739405357c65c2fa9a9298ed07
Maurice Guikema <>
2020-02-27 10:41:23 +0100

    fix: empty field removed from radioselect

--- app/web/documents/forms.py
+++ app/web/documents/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from django.forms import widgets
+from django.forms.widgets import RadioSelect
 from .models import *
 from django.forms.models import inlineformset_factory
 
 
 class DocumentVersionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+        self.fields['uploaded_file'].label = "New Email Label"
+
     class Meta:
         model = DocumentVersion
         exclude = ()
@@ -15,6 +20,18 @@ class DocumentVersionForm(forms.ModelForm):
 
 
 class DocumentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+        self.empty_label=None
+        self.fields['name'].label = "Naam"
+        self.fields['icon'].label = "Icon"
+
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field and isinstance(field , forms.TypedChoiceField):
+                field.choices = field.choices[1:]
+
     class Meta:
         model = Document
         exclude = ()
