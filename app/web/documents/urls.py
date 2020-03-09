a94edac57a4e6e6334b8541992f438d0b280db2e
Maurice Guikema <>
2020-03-09 14:34:17 +0100

    fix: reverse for download version

--- app/web/documents/urls.py
+++ app/web/documents/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('nieuw/<int:document>', DocumentVersionCreate.as_view(), name='add_document_version_to_document'),
     path('nieuw-voor-timeline/<int:moment_id>', CreateDocumentAddToMoment.as_view(), name='add_document_to_moment'),
     path('delete-versie/<int:pk>', DocumentVersionDelete.as_view(), name='documentversion_delete'),
-    path('downloadobject', download_object, name='download_object'),
+    path('download/<int:id>', download_documentversion, name='download_documentversion'),
 ]
