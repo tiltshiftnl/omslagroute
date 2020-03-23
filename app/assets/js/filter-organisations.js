08ea933ebdf3a283e02304575a3244209c9d889a
Anneke Sinnema <mail@annekesinnema.nl>
2020-03-23 15:57:11 +0100

    ⚒ Only add classes, don’t toggle

--- app/assets/js/filter-organisations.js
+++ app/assets/js/filter-organisations.js
@@ -15,23 +15,24 @@ document.addEventListener('click', function (e) {
         var steps = document.querySelectorAll('[data-tags*="' + filter + '"]');
         console.table(steps);
 
-        // find all tags buttons and highlight them too
+        // find all tags buttons
         var tags = document.querySelectorAll('[data-select-targetgroup*="' + filter + '"]');
         console.table(tags);
 
+        // get a container
+        var stepsContainer = document.querySelectorAll(".details-wrapper");
+
         for (var i = 0; i < steps.length; i++) {
-            steps[i].classList.toggle("detail-wrapper--highlighted");
+            steps[i].classList.add("detail-wrapper--highlighted");
         }
 
         for (var i = 0; i < tags.length; i++) {
-            tags[i].classList.toggle("button--tag--selected");
+            tags[i].classList.add("button--tag--selected");
         }
-
-        var stepsContainer = document.querySelectorAll(".details-wrapper");
 
         for (var i = 0; i < stepsContainer.length; i++) {
             if (!stepsContainer[i].classList.contains("detail-wrapper--highlighted")) {
-                stepsContainer[i].classList.toggle("detail-wrapper--passive");
+                stepsContainer[i].classList.add("detail-wrapper--passive");
             }
         }
     }
