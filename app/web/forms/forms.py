a456ee2053d63a64c32529032956603f02d96085
Anneke Sinnema <mail@annekesinnema.nl>
2020-03-27 10:14:37 +0100

    💄 Tweaked output of field containers

--- app/web/forms/forms.py
+++ app/web/forms/forms.py
@@ -28,7 +28,7 @@ class GenericForm(forms.Form):
 
     def as_sections(self):
         return self._html_section_output(
-            normal_row='<div class="form-field" %(html_class_attr)s>%(label)s %(help_text)s %(field)s %(errors)s</div>',
+            normal_row='<div %(html_class_attr)s>%(label)s %(help_text)s %(field)s %(errors)s</div>',
             error_row='%s',
             row_ender='</div>',
             help_text_html=' <p class="help-text">%s</p>',
@@ -131,9 +131,9 @@ class GenericModelForm(forms.ModelForm):
                 # Create a 'class="..."' attribute if the row should have any
                 # CSS classes applied.
                 css_classes = bf.css_classes()
-                css_classes += ' field-%s' % bf.field.widget.__class__.__name__.lower()
+                css_classes += ' form-field--%s' % bf.field.widget.__class__.__name__.lower()
                 if css_classes:
-                    html_class_attr = ' class="%s"' % css_classes
+                    html_class_attr = ' class="form-field %s"' % css_classes
                 if bf.label:
                     label = conditional_escape(bf.label)
                     label = bf.label_tag(label) or ''
