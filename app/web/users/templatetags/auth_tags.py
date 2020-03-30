438044e4b82d5f74be9a08c2eb66705db446c12d
Maurice Guikema <>
2020-03-30 11:10:14 +0200

    fix: user type check for templates

--- app/web/users/templatetags/auth_tags.py
+++ app/web/users/templatetags/auth_tags.py
@@ -2,8 +2,26 @@ from django import template
 from django.contrib.auth.models import Group
 from web.users.auth import auth_test
 register = template.Library()
+from web.users.statics import *
 
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return auth_test(user, group_name)
+
+
+@register.filter(name='is_user_type')
+def is_user_type(user, user_type):
+    return auth_test(user, user_type)
+
+
+@register.filter(name='is_beheerder')
+def is_beheerder(user):
+    return auth_test(user, BEHEERDER)
+
+
+@register.filter(name='is_begeleider')
+def is_begeleider(user):
+    return auth_test(user, BEGELEIDER)
+
+
