f511bb752b438f9aea866bb15e5c84f82262073e
Remco Hoff <remco@code010.nl>
2020-06-02 12:55:46 +0200

    made it look like a modal

--- app/assets/js/vue/CaseStatus.vue
+++ app/assets/js/vue/CaseStatus.vue
@@ -23,47 +23,53 @@
         </p>
         <div class="form-field">
             <button v-if="currentCaseStatus.status !== 3" 
-            v-on:click="setNextStatus(3)"  v-bind:class="setButtonClass(3)" data-handler="modal" data-contentid="modalStatus">
+            v-on:click="setNextStatus(3)"  v-bind:class="setButtonClass(3)">
                 <svg class="check__icon" width="20" height="16">
                     <use href="#check" xlink:href="#alert" width="20" height="18"></use>
                 </svg>
                 Goedkeuren
             </button>
             <button v-if="currentCaseStatus.status !== 2" 
-                v-on:click="setNextStatus(2)" v-bind:class="setButtonClass(2)" data-handler="modal" data-contentid="modalStatus">
+                v-on:click="setNextStatus(2)" v-bind:class="setButtonClass(2)">
                 <svg class="close__icon" width="16" height="16">
                     <use href="#close" xlink:href="#close" width="16" height="16"></use>
                 </svg>
                 Afkeuren
             </button>
             <button v-if="currentCaseStatus.status !== 4" 
-                v-on:click="setNextStatus(4)" v-bind:class="setButtonClass(4)" data-handler="modal" data-contentid="modalStatus">
+                v-on:click="setNextStatus(4)" v-bind:class="setButtonClass(4)">
                 <svg class="close__icon" width="12" height="16">
                     <use href="#pause" xlink:href="#pause" width="12" height="16"></use>
                 </svg>
                 Wacht op GGD
             </button>
         </div>
-        <div role="dialog" class="modal" id="modalStatus">
-            <div v-if="nextCaseStatus.status">
-                <div class="prompt-container show-prompt-approve" data-handler="prompt-approve">
-                    <div class="prompt-approve">
-                        <p>Weet je zeker dat je de status voor <strong>{{ title }}</strong> wilt wijzigen naar <strong>{{ statusText[nextCaseStatus.status] }}</strong>?</p>
-                        <p><strong>{{ emailList }}</strong> ontvangt hiervan een bevestiging per e-mail.</p>
-                        <form>
-                            <div class="form-field form-field--textarea screen-only u-margin-top-2x">
-                                <label for="status-message">Bericht (optioneel)</label>
-                                <textarea id="status-message" name="status-message" cols="40" rows="4"></textarea>
-                            </div>
-                            <span class="helptext">Als je een bericht wil meesturen met in de bevestings e-mail, dan kun je dat hier doen.</span>
-                            <div class="form-field form-field--buttons screen-only u-margin-top-2x">
-                                <button type="button" class="button button--primary"  v-on:click="addCaseStatus()">{{ buttonText[nextCaseStatus.status] }}</button>
-                                <button type="button" class="button button--secondary" v-on:click="setNextStatus(null)" data-handler="modal-close">Annuleren</button>
-                            </div>
-                        </form>
-                    </div>
-
+        <div v-if="nextCaseStatus.status" class="container-modal--vue">
+            <a href="#" class="modal-close--bg" v-on:click="setNextStatus(null)" ></a>
+            <div class="prompt-container show-prompt-approve" data-handler="prompt-approve">
+                <button v-on:click="setNextStatus(null)" class="button button--close">
+                    <svg width="20" height="20" title="Sluit venster">
+                        <use href="#close" xlink:href="#close" width="20" height="20"></use>
+                    </svg>
+                    <span class="sr-only">Sluit venster</span>
+                </button>
+                <h2>Status wijzigen</h2>
+                <div class="prompt-approve">
+                    <p>Weet je zeker dat je de status voor <strong>{{ title }}</strong> wilt wijzigen naar <strong>{{ statusText[nextCaseStatus.status] }}</strong>?</p>
+                    <p><strong>{{ emailList }}</strong> ontvangt hiervan een bevestiging per e-mail.</p>
+                    <form>
+                        <div class="form-field form-field--textarea screen-only u-margin-top-2x">
+                            <label for="status-message">Bericht (optioneel)</label>
+                            <textarea id="status-message" name="status-message" cols="40" rows="4"></textarea>
+                        </div>
+                        <span class="helptext">Als je een bericht wil meesturen met in de bevestings e-mail, dan kun je dat hier doen.</span>
+                        <div class="form-field form-field--buttons screen-only u-margin-top-2x">
+                            <button type="button" class="button button--primary"  v-on:click="addCaseStatus()">{{ buttonText[nextCaseStatus.status] }}</button>
+                            <button type="button" class="button button--secondary" v-on:click="setNextStatus(null)" data-handler="modal-close">Annuleren</button>
+                        </div>
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
@@ -159,7 +165,6 @@ export default {
         },
         addCaseStatus: function () {
             this.loading = true;
-            console.log("nextCaseStatus", this.nextCaseStatus);
             axios.post(`/api/casestatus/`, this.nextCaseStatus)
                 .then((response) => {
                     this.loading = false;
