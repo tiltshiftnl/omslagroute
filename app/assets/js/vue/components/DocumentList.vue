62a74c4a82c30e29ff5189e3c7423c2dbf8c6959
Maurice Guikema <mguikema@gmail.com>
2020-06-08 13:51:52 +0200

    vue components added

---
+++ app/assets/js/vue/components/DocumentList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <header>
+            <h2 class="h3">Bijlagen <small>({{ documentList.length }})</small></h2>
+        </header>
+        <ul class="list-as-table">
+            <li v-for="document in documentList" :key="document.id">
+                <div>
+                    <a :href="document.url" target="_blank">
+                        <span>{{ document.name }}</span>
+                    </a>
+                    <span>{{ document.extension }}</span>
+                    <span>{{ document.uploaded }}</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import { mapState } from 'vuex'
+
+export default {
+    name: "DocumentList",
+    computed: {
+        ...mapState([
+        'documentList'
+        ]),
+    },
+    methods: {}
+};
+</script>
---
+++ app/assets/js/vue/components/SectionCase.vue
@@ -0,0 +1,60 @@
+<template>
+<section class="section section--client">
+    <h1>{{ caseObject.client_name }}</h1>
+    <h2 class="h3">{{ form }}</h2>
+    <p class="print-only">
+    </p>
+
+
+    <section class="section-details-client">
+
+        <!-- START BIJLAGE -->
+        <div class="details-client details-client--appendix u-width-50">
+            <document-list></document-list>
+        </div>
+
+        <div class="details-client details-client--approve u-width-50">
+            <case-status></case-status>
+        </div>
+        <!-- END BIJLAGE -->
+        
+
+    </section>
+    <section class="section section--forms">
+        <!-- START Client basis gegevens -->
+        <div class="details-client">
+            <header>
+                <h2 class="h3">Gegevens</h2>
+            </header>
+
+            <dl>
+
+            </dl>
+        </div>
+        <!-- END Client basis gegevens -->
+
+    </section>
+</section>
+</template>
+
+<script>
+import CaseStatus from './CaseStatus.vue'
+import DocumentList from './DocumentList.vue'
+import { mapState } from 'vuex';
+
+export default {
+    name: "SectionCase",
+    data: () => ({}),
+    computed: {
+        ...mapState([
+            'caseObject',
+            'form',
+        ])
+    },
+    components: {
+        'document-list': DocumentList,
+        'case-status': CaseStatus,
+    },
+    methods: {}
+};
+</script>
