ede4d340c045d71c52b6b80037c24cc0be0349af
Maurice Guikema <mguikema@gmail.com>
2020-06-08 16:14:17 +0200

    feat: case status history for wonen

--- app/assets/js/case-status.js
+++ app/assets/js/case-status.js
@@ -3,12 +3,41 @@
 import Vue from "vue";
 import CaseStatus from "./vue/CaseStatus.vue";
 import axios from "axios";
+import VueLuxon from "vue-luxon";
 
 const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value');
 
 axios.defaults.headers.post['X-CSRFToken'] = csrfToken;
 axios.defaults.headers.put['X-CSRFToken'] = csrfToken;
 axios.defaults.headers.delete['X-CSRFToken'] = csrfToken;
+
+Vue.use(VueLuxon, {
+    serverZone: 'utc',
+    serverFormat: 'iso',
+    clientZone: 'Europe/Amsterdam',
+    clientFormat: 'locale',
+    localeLang: 'nl-NL',
+    localeFormat: {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    }, // see localeFormat below
+    diffForHumans: {}, // see diffForHumans below
+    i18n: {
+        lang: "nl-NL",
+        year: "[one]jaar|[other]jaren",
+        month: "[one]maand|[other]maanden",
+        week: "[one]week|[other]weken",
+        day: "[one]dag|[other]dagen",
+        hour: "[one]uur|[other]uren",
+        minute: "[one]minuut|[other]minuten",
+        second: "[one]seconde|[other]seconden",
+        ago: 'geleden',
+        in: 'over',
+    } // see i18n below
+});
 
 new Vue({
  el: "#app",
--- app/web/cases/serializers.py
+++ app/web/cases/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from .models import CaseStatus
+from web.profiles.models import Profile
+from web.users.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Profile
+        fields = ['user']
 
 
 class CaseStatusSerializer(serializers.ModelSerializer):
+    # profile = ProfileSerializer()
+    username = serializers.CharField(read_only=True, source="profile.user.username")
+
     class Meta:
         model = CaseStatus
         fields = '__all__'
